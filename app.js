5572b0704d19ade7316f2d89400bb0c42eb5fb13
Altaf Dasurkar <altaf.dkr@gmail.com>
2018-12-14 14:16:41 -0500

    Train Scheduler App

---
+++ app.js
@@ -0,0 +1,66 @@
+// Firebase Settings
+  var config = {
+    apiKey: "AIzaSyDHllXHQGNLNwC4NB8QijMSlNrV3sp_4yo",
+    authDomain: "trainscheduler-f404a.firebaseapp.com",
+    databaseURL: "https://trainscheduler-f404a.firebaseio.com",
+    projectId: "trainscheduler-f404a",
+    storageBucket: "trainscheduler-f404a.appspot.com",
+    messagingSenderId: "973777506341"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$('#add-train').on("click", function (event) {
+    event.preventDefault();
+
+    var addTrain = $('#trainName').val().trim();
+    var destination = $('#destination').val().trim();
+    var firstTime = $("#firstTrain").val().trim();
+    var frequency = $("#frequency").val().trim();
+
+    var newTrain = {
+        name: addTrain,
+        destination: destination,
+        firstTime: firstTime,
+        frequency: frequency
+    };
+
+    database.ref().push(newTrain);
+    console.log(newTrain.name);
+
+    alert("Train was successfully added!");
+
+    $('#trainName').val("");
+    $('#destination').val("");
+    $("#firstTrain").val("");
+    $("#frequency").val("");
+});
+
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+    
+    var addTrain = childSnapshot.val().name;
+    var destination = childSnapshot.val().destination;
+    var firstTime = childSnapshot.val().firstTime;
+    var frequency = childSnapshot.val().frequency;
+
+    var timeArray= firstTime.split(":")
+    var trainTime = moment().hours(timeArray[0]).minutes(timeArray[1]);
+    console.log(trainTime);
+
+    var nextTrain = moment().diff(trainTime, "minutes");
+    var minutesAway = nextTrain % frequency;
+  var timeToNextTrain = frequency - minutesAway;
+  var nextArrival = moment().add(timeToNextTrain, "minutes")
+
+    var newRow = $("<tr>").append(
+        $("<td>").text(addTrain),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td>").text(nextArrival),
+        $("<td>").text(timeToNextTrain)
+        
+    );
+    $("#insert-schedule > tbody").append(newRow);
+});
